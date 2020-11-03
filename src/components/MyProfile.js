e005740ab768e1d4fd48b086b98db7921360838a
nynm94 <70739842+nynm94@users.noreply.github.com>
2020-11-03 21:50:00 +0800

    Update MyProfile.js

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -11,38 +11,35 @@ class MyProfile extends Component {
             <h2>My Profile</h2>
             <br></br>
             <Form>
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control value="Tan Wei Jian" disabled/>
-                    </Form.Group>
+                <Form.Group controlId="formName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control value="Tan Wei Jian" disabled/>
+                </Form.Group>
 
-                    <Form.Group as={Col} controlId="formNRIC">
-                        <Form.Label>NRIC</Form.Label>
-                        <Form.Control value="SXXXX928C" disabled/>
-                    </Form.Group>
-                </Form.Row>
+                <Form.Group controlId="formNRIC">
+                    <Form.Label>NRIC</Form.Label>
+                    <Form.Control value="SXXXX928C" disabled/>
+                </Form.Group>
 
                 <Form.Group controlId="formAddress">
                     <Form.Label>Address</Form.Label>
                     <Form.Control value="1234 Main St" />
                 </Form.Group>
 
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formContact">
-                        <Form.Label>Contact</Form.Label>
-                        <Form.Control value="91248543"/>
-                    </Form.Group>
+                <Form.Group controlId="formContact">
+                    <Form.Label>Contact</Form.Label>
+                    <Form.Control value="91248543"/>
+                </Form.Group>
 
-                    <Form.Group as={Col} controlId="formUnit">
-                        <Form.Label>Unit</Form.Label>
-                        <Form.Control value="EMT" disabled/>
-                    </Form.Group>
-                </Form.Row>
+                <Form.Group controlId="formUnit">
+                    <Form.Label>Unit</Form.Label>
+                    <Form.Control value="EMT" disabled/>
+                </Form.Group>
 
                 <Button variant="primary" type="submit">
                     Save
                 </Button>
+                
             </Form>
         </span>);
     }
