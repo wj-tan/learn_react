bf51d0f7c7b40558d901c2faac112f959643cc92
jiachengng <1901825.sit.singaporetech.edu.sg>
2020-11-03 21:16:36 +0800

    Create Register.js

---
+++ src/components/Register.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            
+         }
+    }
+    render() { 
+        return ( <span>
+            <h2>Register</h2>
+        </span> );
+    }
+}
+ 
+export default Register;
