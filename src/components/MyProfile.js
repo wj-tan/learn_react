1c1c1bd554ad422eaaae1532dcfb7f7598c4d076
nynm94 <70739842+nynm94@users.noreply.github.com>
2020-11-07 19:05:36 +0800

    Update MyProfile.js

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Form, Button, Col, Row, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row'
 
 class MyProfile extends Component {
     constructor(props) {
@@ -8,37 +10,51 @@ class MyProfile extends Component {
     }
     render() {
         return (<span>
+            <br></br>
             <h2>My Profile</h2>
             <br></br>
             <Form>
-                <Form.Group controlId="formName">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control value="Tan Wei Jian" disabled/>
-                </Form.Group>
+                <Col md-6>
+                    <Form.Group controlId="formName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control value="Eye Drop King Tan Wei Jian" disabled/>
+                    </Form.Group>
+                </Col>
 
-                <Form.Group controlId="formNRIC">
-                    <Form.Label>NRIC</Form.Label>
-                    <Form.Control value="SXXXX928C" disabled/>
-                </Form.Group>
+                <Form.Row>
+                    <Col md-3>
+                        <Form.Group as={Col} controlId="formNRIC">
+                            <Form.Label>NRIC</Form.Label>
+                            <Form.Control value="SXXXX928C" disabled/>
+                        </Form.Group>
+                    </Col>
+                    <Col md-3>
+                        <Form.Group as={Col} controlId="formUnit">
+                            <Form.Label>Unit</Form.Label>
+                            <Form.Control value="EMT" disabled/>
+                        </Form.Group>
+                    </Col>
+                </Form.Row>
 
-                <Form.Group controlId="formAddress">
-                    <Form.Label>Address</Form.Label>
-                    <Form.Control value="1234 Main St" />
-                </Form.Group>
-
-                <Form.Group controlId="formContact">
-                    <Form.Label>Contact</Form.Label>
-                    <Form.Control value="91248543"/>
-                </Form.Group>
-
-                <Form.Group controlId="formUnit">
-                    <Form.Label>Unit</Form.Label>
-                    <Form.Control value="EMT" disabled/>
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    Save
-                </Button>
+                <Col md-6>
+                    <Form.Group controlId="formAddress">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control defaultValue="1234 Main St" />
+                    </Form.Group>
+                </Col>
+                
+                <Col md-6>
+                    <Form.Group controlId="formContact">
+                        <Form.Label>Contact</Form.Label>
+                        <Form.Control defaultValue="91248543"/>
+                    </Form.Group>
+                </Col>
+          
+                <Col md-6>
+                    <Button variant="primary" size="lg" type="submit" block>
+                        Save
+                    </Button>
+                </Col>
             </Form>
         </span>);
     }
