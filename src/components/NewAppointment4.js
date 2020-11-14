81c213df3a345c5fea89062cf8a78c44d9a418c7
nynm94 <70739842+nynm94@users.noreply.github.com>
2020-11-14 17:11:23 +0800

    Update NewAppointment4.js

--- src/components/NewAppointment4.js
+++ src/components/NewAppointment4.js
@@ -1,97 +1,55 @@
 import React, { Component } from 'react';
 import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import Col from 'react-bootstrap/Col';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 
-class NewAppointment3 extends Component {
+class NewAppointment4 extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
+    }
+    handleclick(){
+        this.props.handleAddAppointment(this.props.tempAppointment)
     }
     render() { 
         return ( 
             <span>
             <br></br>
-                <h2>New Appointment</h2>
+                <h2>Appointment Confirmation</h2>
                 <br></br>
                 <Form>
                     <Col>
                         <Form.Group controlId="formBranch">
                             <Form.Label>Branch : </Form.Label>
-                            <Form.Control value="Home Team Academy" disabled></Form.Control>
+                            <Form.Control value={this.props.tempAppointment.branch}disabled></Form.Control>
                         </Form.Group>
                     </Col>
                     
                     <Col>
                         <Form.Group controlId="formPurpose">
-                            <Form.Label>Purpose of Visit</Form.Label>
-                            <Form.Control as="select"></Form.Control>
-                            
+                            <Form.Label>Purpose : </Form.Label>
+                            <Form.Control value={this.props.tempAppointment.purpose}disabled></Form.Control>
                         </Form.Group>
                     </Col>
         
                     <Col>
                         <Form.Group controlId="formDate">
-                            <Form.Label>Date</Form.Label><br></br>
-                            <input type="date"></input>
+                            <Form.Label>Date : </Form.Label>
+                            <Form.Control value={this.props.tempAppointment.date}disabled></Form.Control>
                         </Form.Group>
                     </Col>
                     
                     <Col>
                         <Form.Group controlId="formTime">
-                            <Form.Label>Time</Form.Label><br></br>
-                            <Form.Row>
-                                <Col>
-                                    <Form.Check
-                                        inline
-                                        type="checkbox"
-                                        label="10AM"
-                                    />
-                                    <Form.Check
-                                        inline
-                                        type="checkbox"
-                                        label="11AM"
-                                    />
-                                </Col>
-                            </Form.Row>
-
-                            <Form.Row>
-                                <Col>
-                                    <Form.Check
-                                        inline
-                                        type="checkbox"
-                                        label="2PM"
-                                    />
-                                    <Form.Check
-                                        inline
-                                        type="checkbox"
-                                        label="3PM"
-                                    />
-                                </Col>
-                            </Form.Row>
-
-                            <Form.Row>
-                                <Col>
-                                    <Form.Check
-                                        inline
-                                        type="checkbox"
-                                        label="4PM"
-                                    />
-                                    <Form.Check
-                                        inline
-                                        type="checkbox"
-                                        label="5PM"
-                                    />
-                                </Col>
-                            </Form.Row>
-
+                            <Form.Label>Time : </Form.Label>
+                            <Form.Control value={this.props.tempAppointment.time}disabled></Form.Control>
                         </Form.Group>
                     </Col>
+
                     <Col>
-                    <Button variant="primary" type="submit" block>
-                        Conform Booking
-                    </Button>
-                </Col>
+                        <NavLink to="/Home" onClick={() => this.handleclick()}><Button variant="primary" type="submit" block>Confirm Booking</Button></NavLink>
+                    </Col>
 
                 </Form>
             </span>
@@ -99,4 +57,4 @@ class NewAppointment3 extends Component {
     }
 }
  
-export default NewAppointment3;
+export default NewAppointment4;
