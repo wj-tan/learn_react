08fc755566eac1ff9e559935cc6d460a56e9acc9
jiachengng <1901825.sit.singaporetech.edu.sg>
2020-11-15 03:18:45 +0800

    Update MyAppointment.js

--- src/components/MyAppointment.js
+++ src/components/MyAppointment.js
@@ -7,8 +7,19 @@ import Row from 'react-bootstrap/Row';
 class MyAppointment extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        var date = new Date().getDate(); //To get the Current Date
+        var month = new Date().getMonth() + 1; //To get the Current Month
+        var year = new Date().getFullYear(); //To get the Current Year
+        var hours = new Date().getHours(); //To get the Current Hours
+        var min = new Date().getMinutes(); //To get the Current Minutes
+        // var obDate = new Date.parse((month+date+year));
+        // var obDate = new Date(year+"-"+month+"-"+date);
+        var myDate = new Date(year + "-" + month + "-" + date);
+        var date2 = new Date().getDate() + 1;
 
+        this.state = {
+            selectedMode: "",
+            currentTime: (year + "-" + month + "-" + date).toString()
         }
     }
 
@@ -19,12 +30,110 @@ class MyAppointment extends Component {
         <span>
             <h1>My Appointment(s)</h1>
             <br></br>
+            <div id="branch">
+                <Row>
+                    <Col>
+                        <Button variant="primary" size="lg" onClick={() => this.setState({ selectedMode: 'Upcoming' })}>Upcoming</Button>
+                    </Col>
+                    <Col>
+                        <Button variant="primary" size="lg" onClick={() => this.setState({ selectedMode: 'Past' })}>Past</Button>
+                    </Col>
+                    <Col>
+                        <Button variant="primary" size="lg" onClick={() => this.setState({ selectedMode: 'All' })}>All</Button>
+                    </Col>
+                </Row>
+            </div>
             <div id = 'center'>
-                    
                         <br></br>
                         <br></br>
                         <br></br>
-                        
+                        {(() => {
+                    switch (this.state.selectedMode) {
+                        case 'Upcoming':
+                            return (<Table id="tablex">
+                                <thead>
+                                    <tr>
+                                        <th>S/N</th>
+                                        <th>Branch</th>
+                                        <th>Date</th>
+                                        <th>Time</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    {(this.props.appointments) && this.props.appointments.filter(appointment => (appointment.date >= this.state.currentTime) ).map((appointment, index) => {
+                                        return (
+                                            <tr>
+                                                <td>{index + 1}</td>
+                                                <td>{appointment.branch}</td>
+                                                <td>{appointment.date}</td>
+                                                <td>{appointment.time}</td>
+                                                <td><NavLink to="/ManageAppointment" ><Button onClick={() => this.props.handleSelect(index)}>Manage</Button></NavLink></td>
+                                            </tr>
+                                        )
+                                    })}
+
+                                </tbody>
+
+                            </Table>);
+
+                        case 'Past':
+                            return (<Table>
+                                <thead>
+                                    <tr>
+                                        <th>S/N</th>
+                                        <th>Branch</th>
+                                        <th>Date</th>
+                                        <th>Time</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    {(this.props.appointments) && this.props.appointments.filter(appointment => (appointment.date < this.state.currentTime)).map((appointment, index) => {
+                                        return (
+                                            <tr>
+                                                <td>{index + 1}</td>
+                                                <td>{appointment.branch}</td>
+                                                <td>{appointment.date}</td>
+                                                <td>{appointment.time}</td>
+                                                <td><NavLink to="/ManageAppointment" ><Button onClick={() => this.props.handleSelect(index)}>Manage</Button></NavLink></td>
+                                            </tr>
+                                        )
+                                    })}
+
+                                </tbody>
+
+                            </Table>);
+                        case 'All':
+                            return (<Table>
+                                <thead>
+                                    <tr>
+                                        <th>S/N</th>
+                                        <th>Branch</th>
+                                        <th>Date</th>
+                                        <th>Time</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    {(this.props.appointments) && this.props.appointments.map((appointment,index)=>{
+                                        return (
+                                            <tr>
+                                                <td>{index + 1}</td>
+                                                <td>{appointment.branch}</td>
+                                                <td>{appointment.date}</td>
+                                                <td>{appointment.time}</td>
+                                                <td><NavLink to="/ManageAppointment" ><Button onClick={() => this.props.handleSelect(index)}>Manage</Button></NavLink></td>
+                                            </tr>
+                                        )
+                                    })} 
+                                </tbody>
+                            </Table> )
+                        default:
+                            return(
                             <Table>
                                 <thead>
                                     <tr>
@@ -49,7 +158,11 @@ class MyAppointment extends Component {
                                         )
                                     })} 
                                 </tbody>
-                            </Table> 
+                            </Table> )
+                    }
+                })()}
+                        
+                            
             </div>
         </span> );
     }
