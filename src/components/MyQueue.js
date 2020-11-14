a9db0bfb1795d1c70149838509c2895c0820e080
nynm94 <70739842+nynm94@users.noreply.github.com>
2020-11-15 04:12:33 +0800

    Create MyQueue.js

---
+++ src/components/MyQueue.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Form, Button, FormGroup, FormControl, ControlLabel, Table, ToastBody } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+//created by typing cc enter
+class MyQueue extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+    render() {
+        return '';
+    }
+}
+
+export default MyQueue;
